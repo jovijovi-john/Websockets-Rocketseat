b575db5c67284f2b4ade43d0f58c1227ffea9ba7
John <jovijovi2019@hotmail.com>
2023-05-25 13:31:26 -0300

    fix: TUDO

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io("localhost://3000");
+const socket = io("http://localhost:3000");
 
 const urlSearch = new URLSearchParams(window.location.search);
 const username = urlSearch.get("username");
